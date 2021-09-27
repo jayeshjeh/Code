b52b543b1abf2442b84146b85930d7ec2f49da80
jayeshjeh <jay.parmar.11169@gmail.com>
2021-09-27 20:18:57 +0530

    commit

---
+++ prgm65.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int SumOdd (int Arr[], int iSize)
+{
+    int i=0,iSum=0,iCnt=0;
+
+    for(i=0;i<iSize;i++)
+    {
+        if(Arr[i]>10)
+        {
+            iCnt++;
+        }
+    }
+    return iCnt;
+    
+}
+int main()
+{
+    int iLength =0, i=0,iRet=0;
+    int*ptr=NULL;
+    
+
+    printf("Enter number of elements\n");
+    scanf("%d",&iLength);
+
+    ptr= (int*)malloc(sizeof(int)*iLength);
+    printf("Enter elements\n");
+    for(i=0;i<iLength;i++)
+    {
+        scanf("%d",&ptr[i]);
+    }
+    iRet = SumOdd (ptr,iLength);
+    printf("Addition of odd elements are  is :%d\n",iRet);
+    free(ptr);
+    return 0;
+}

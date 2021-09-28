fc5671a9888d40da89fea0cc6dbee68d06ff2f28
jayeshjeh <jay.parmar.11169@gmail.com>
2021-09-28 21:09:08 +0530

    commit

---
+++ prgm74.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    char Arr[11]="Marvellous";
+    printf("%c\n",Arr[0]);
+    printf("%c\n",Arr[5]);
+    printf("%c\n",Arr[9]);
+                            //Output M l s
+    return 0;
+}

794d9b3d8cab7a1120bca0089af3e1840fe86b62
jayeshjeh <jay.parmar.11169@gmail.com>
2021-09-28 19:00:37 +0530

    commit

---
+++ prgm71.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+bool CheckNumber(int Arr[],int iSize,int iNo)
+{
+    int i=0;
+    bool bAns = false;
+    for(i=0;i<iSize;i++)
+    {
+        if(Arr[i]==iNo)
+        {
+            break;
+        }
+    }
+    //Loop var Sampala///
+    if(i==iSize)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+    
+
+}
+
+int main()
+{
+    int iLength=0,i=0, iValue =0;
+    int *ptr=NULL;
+    bool bRet=false;
+
+    printf("Enter number of elements\n");
+    scanf("%d",&iLength);
+
+    ptr=(int*)malloc(sizeof(int)*iLength);
+    printf("Enter elements\n");
+    for(i=0;i<iLength;i++)
+    {
+        scanf("%d",&ptr[i]);
+    }
+    printf("Enter elements\n");
+    scanf("%d",&iValue);
+    bRet = CheckNumber(ptr,iLength,iValue);
+    if(bRet== true)
+    {
+        printf("Number is there\n");
+    }
+    else
+    {
+        printf("Number is not there\n");
+    }
+    free(ptr);
+    return 0;
+}

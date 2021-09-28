ee9ffa8a5efa6011119ee48aadb77208a17448dc
jayeshjeh <jay.parmar.11169@gmail.com>
2021-09-28 20:05:43 +0530

    commit

---
+++ prgm73.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h> // Last occurance of element
+
+int LastOccurance(int Arr[],int iSize,int iNo)
+{
+    int i=0,index = -1;
+    for(i=0-1;i<iSize;i++)
+    {
+        if(Arr[i]==iNo)
+        {
+            index = i;
+        }
+    }           
+    return index;
+}
+int main()
+{
+    int iLength=0,i=0, iValue =0,iRet=0;
+    int *ptr=NULL;
+    printf("Enter number of elements\n");
+    scanf("%d",&iLength);
+
+    ptr=(int*)malloc(sizeof(int)*iLength);
+    printf("Enter elements\n");
+    for(i=0;i<iLength;i++)
+    {
+        scanf("%d",&ptr[i]);
+    }
+    printf("Enter element to search\n");
+    scanf("%d",&iValue);
+    iRet = LastOccurance(ptr,iLength,iValue);
+    if(iRet== -1)
+    {
+        printf("There is no such number\n");
+    }
+    else
+    {
+        printf("Number is there at index %d\n",iRet);
+    }
+    free(ptr);
+    return 0;
+}

237549a14c3ffc6c0b78cf05f6294ee24061100c
jayeshjeh <jay.parmar.11169@gmail.com>
2021-09-30 23:33:45 +0530

    commit

--- prgm1.c
+++ prgm1.c
@@ -1,3 +1,5 @@
+#include<stdio.h>
+
 int Addition (int iNo1, int iNo2)
 {
     return iNo1 + iNo2;
@@ -8,5 +10,11 @@ int main()
     int x, y, z;
     
     printf("Enter first number\n");
-    scanf("")
+    scanf("%d",&x);
+    printf("Enter second number\n");
+    scanf("%d",&y);
+
+    z = Addition(x,y);
+    printf("Addition is : %d\n",z);
+    return 0;
 }

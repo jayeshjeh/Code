d6964e18802606f6438d9374d5e46512ab3e006e
jayeshjeh <jay.parmar.11169@gmail.com>
2021-10-02 00:06:17 +0530

    commit

--- 3.5.c
+++ 3.5.c
@@ -1,3 +1,38 @@
 #include<stdio.h>
 #include<stdbool.h>
-typedef int 
+typedef int BOOL;
+#define TRUE 1
+#define FALSE 0
+
+int ChkVowel(char c)
+{
+    if(c=='a'||'e'||'i'||'o'||'u')
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+
+int main()
+{
+    char cValue = '\0';
+    BOOL bRet = FALSE;
+
+    printf("Enter Character\n");
+    scanf("%c",&cValue);
+
+    bRet= ChkVowel(cValue);
+
+    if(bRet==TRUE)
+    {
+        printf("It is Vowel\n");
+    }
+    else
+    {
+        printf("It is not Vowel\n");
+    }
+    return 0;
+}

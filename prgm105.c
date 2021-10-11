2b57e2a4489b0141b227740b348e74896a026cb5
jayeshjeh <jay.parmar.11169@gmail.com>
2021-10-11 20:52:45 +0530

    commit

---
+++ prgm105.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void strrevX(char *str)
+{
+
+}
+int main()
+{
+    char Arr[30];
+
+    printf("Enter string\n");
+    scanf("%d[^'\n']s",Arr);
+
+    strrevX(Arr);
+    printf("Reverse string is : %s\n",Arr);
+
+    return 0;
+}

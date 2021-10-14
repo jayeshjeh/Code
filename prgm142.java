7e5f3b39112a6e25191e85a3d9ccfb730a526984
jayeshjeh <jay.parmar.11169@gmail.com>
2021-10-14 14:34:33 +0530

    commit

---
+++ prgm142.java
@@ -0,0 +1,71 @@
+import java.util.*;
+class Marvellous
+{
+	public boolean CheckArmstrong(int iNo)
+	{
+		int iTemp, iDigCnt = 0, iDigit, iPower = 1;
+		int iCnt, iSum = 0;
+		if(iNo < 0)
+		{
+			iNo = -iNo;
+		}
+		iTemp = iNo;
+
+		while(iTemp != 0)
+		{
+			iDigCnt++;
+			iTemp = iTemp / 10;
+		}
+
+		iTemp = iNo;
+
+		while(iTemp != 0)
+		{
+			iDigit = iTemp % 10;
+			for(iCnt = 1; iCnt <= iDigCnt; iCnt++)
+			{
+				iPower = iPower * iDigit;
+			}
+			iSum = iSum + iPower;
+			iPower = 1; 
+			iTemp = iTemp / 10;
+		}
+		if(iSum == iNo)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
+class Program142
+{
+	public static void main(String[] arg)
+	{
+		Scanner sobj = new Scanner(System.in);
+		int iValue;
+		boolean bRet;
+
+		System.out.println("Enter number");
+		iValue = sobj.nextInt();
+
+		Marvellous mobj = new Marvellous();
+		bRet = mobj.CheckArmstrong(iValue);
+
+		if(bRet == true)
+		{	System.out.println("It is Armstrong number");	}
+		else
+		{	System.out.println("It is not Armstrong number");	}
+	}
+}
+
+
+
+
+
+
+
+
+

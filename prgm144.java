44e9e9ffde4f4026822f5ccc5f47a5c2078e8b69
jayeshjeh <jay.parmar.11169@gmail.com>
2021-10-18 19:32:52 +0530

    commit

--- prgm144.java
+++ prgm144.java
@@ -1,11 +1,10 @@
 import java.util.*;     //to take input
-
-class Program143
+class Program144
 {
     public static void main(String arg[])
     {
         Scanner sobj = new Scanner(System.in);
-        int iSize=0,iCnt =0;
+        int iSize=0,iCnt =0,iRet=0;
 
         System.out.println("Enter Number of elements");
         iSize=sobj.nextInt();
@@ -17,11 +16,22 @@ class Program143
         {
             Arr[iCnt]=sobj.nextInt();
         }
-        System.out.println("Entered elements are");
-        for(iCnt =0;iCnt<Arr.length;iCnt++)
+        Marvellous mobj = new Marvellous();
+        iRet=mobj.Add(Arr);
+
+        System.out.println("Addition is : "+iRet);
+    }
+}
+class Marvellous
+{
+    public int Add(int Brr[])
+    {
+        int iSum=0,iCnt=0;
+        for(iCnt=0;iCnt<Brr.length;iCnt++)
         {
-            System.out.println(Arr[iCnt]);
+            iSum = iSum+Brr[iCnt];
         }
+        return iSum;
     }
 }
 

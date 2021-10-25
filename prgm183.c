b226991f2224c0b36f8fdce468558e04e9225a1e
jayeshjeh <jay.parmar.11169@gmail.com>
2021-10-25 21:34:06 +0530

    commit

---
+++ prgm183.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
+
+int main()
+{
+    int fd =0, iRet=0;
+    char Fname[20];
+
+
+    printf("Enter file name\n");
+    scanf("%s",Fname);
+
+   fd = open(Fname,O_RDWR);
+   if(fd== -1)
+   {
+        printf("Unable to open the file\n");
+        return -1;
+   }
+    iRet = lseek(fd,0,2);
+
+    printf("File size is  : %d\n",iRet);
+    close(fd);
+    return 0;
+}

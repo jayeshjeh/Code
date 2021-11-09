1fbe57d3c658fb4e1f32631013d91e5748392590
jayeshjeh <jay.parmar.11169@gmail.com>
2021-11-09 21:14:24 +0530

    commit

--- prgm205.c
+++ prgm205.c
@@ -11,25 +11,68 @@ typedef struct node NODE;
 typedef struct node* PNODE;
 typedef struct node** PPNODE;
 
+void InsertFirst(PPNODE Head, int no)
+{
+    PNODE newn = NULL;
+    newn = (PNODE)malloc(sizeof(NODE));    //1st - Allocate the memory
+
+    newn->data = no;                        //Initialise the node
+    newn->next = NULL;
+
+    if(*Head = NULL)
+    {
+        *Head = newn;
+    }
+    else
+    {
+        *
+    }
+}
+
+void InsertLast(PPNODE Head, int no)
+{
+    PNODE newn = NULL;
+    PNODE temp = *Head;
+    newn = (PNODE)malloc(sizeof(NODE));    //1st - Allocate the memory
+    newn->data = no;                        //Initialise the node
+    newn->next = NULL;
+
+    if(*Head = NULL)
+    {
+        *Head = newn;
+    }
+    else
+    {
+        while(temp->next != NULL)
+        {
+            temp = temp -> next;
+        }
+        temp->next = newn;
+    }
+}
 
 int main()
 {
     PNODE first = NULL;
 
+    InsertFirst(&first,51);     //InsertFirst(50,51)
+    InsertFirst(&first,21);     //InsertFirst(50,21)
+    InsertFirst(&first,11);     //InsertFirst(50,11)
+
     return 0;
 }
 
 /*
-    InsertFirst(PPNODE Head, int no)
-    InsertLast(PPNODE Head, int no)
-    InsertAtPos(PPNODE Head, int no, int pos)
+    void InsertFirst(PPNODE Head, int no)
+    void InsertLast(PPNODE Head, int no)
+    void InsertAtPos(PPNODE Head, int no, int pos)
 
-    DeleteFirst(PPNODE Head)
-    DeleteLast(PPNODE Head)
-    DeleteAtPos(PPNODE Head, int Pos)
+    void DeleteFirst(PPNODE Head)
+    void DeleteLast(PPNODE Head)
+    void DeleteAtPos(PPNODE Head, int Pos)
 
-    Display(PNODE Head)
-    Count(PNODE Head)
+    void Display(PNODE Head)
+    int Count(PNODE Head)
 */
 
  

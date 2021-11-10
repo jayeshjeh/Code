4e02fe6c916f292cd459961825029a608df0d701
jayeshjeh <jay.parmar.11169@gmail.com>
2021-11-10 18:39:10 +0530

    commit

--- prgm205.c
+++ prgm205.c
@@ -4,22 +4,23 @@
 struct node
 {
     int data;
-    struct node* next;
+    struct node *next;
 };
 
 typedef struct node NODE;
-typedef struct node* PNODE;
-typedef struct node** PPNODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
 
 void InsertFirst(PPNODE Head, int no)
 {
     PNODE newn = NULL;
-    newn = (PNODE)malloc(sizeof(NODE));    //1st - Allocate the memory
 
-    newn->data = no;                        //Initialise the node
-    newn->next = NULL;
+    newn = (PNODE)malloc(sizeof(NODE));
 
-    if(*Head = NULL)
+    newn->data =no;
+    newn-> next = NULL;
+
+    if(*Head == NULL)
     {
         *Head = newn;
     }
@@ -35,20 +36,20 @@ void InsertLast(PPNODE Head, int no)
     PNODE newn = NULL;
     PNODE temp = *Head;
 
-    newn = (PNODE)malloc(sizeof(NODE));    //1st - Allocate the memory
-    
-    newn->data = no;                        //Initialise the node
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = no;
     newn->next = NULL;
 
-    if(*Head = NULL)
+    if(*Head == NULL)
     {
         *Head = newn;
     }
     else
     {
-        while(temp->next != NULL)
+        while(temp->next =! NULL)
         {
-            temp = temp -> next;
+            temp = temp->next;
         }
         temp->next = newn;
     }
@@ -63,9 +64,9 @@ void Display(PNODE Head)
     }
 }
 
-int Count(PNODE Head)
+void Count(PNODE Head)
 {
-    int iCnt =0;
+    int iCnt = 0;
     while(Head != NULL)
     {
         iCnt++;
@@ -74,36 +75,13 @@ int Count(PNODE Head)
     return iCnt;
 }
 
+
 int main()
 {
     PNODE first = NULL;
-    int iRet =0;
 
-    InsertFirst(&first,101);
-    InsertFirst(&first,51);     //InsertFirst(50,51)
-    InsertFirst(&first,21);     //InsertFirst(50,21)
-    InsertFirst(&first,11);     //InsertFirst(50,11)
-
-    InsertLast(&first,111);
-    Display(first);
-    iRet = Count(first);
-
-    printf("\n Number of elements are : %d\n",iRet);
-
-    return 0;
+    InsertFirst(&first,51);
+    InsertFirst(&first,21);
+    InsertFirst(&first,11);
 }
 
-/*
-    void InsertFirst(PPNODE Head, int no)
-    void InsertLast(PPNODE Head, int no)
-    void InsertAtPos(PPNODE Head, int no, int pos)
-
-    void DeleteFirst(PPNODE Head)
-    void DeleteLast(PPNODE Head)
-    void DeleteAtPos(PPNODE Head, int Pos)
-
-    void Display(PNODE Head)
-    int Count(PNODE Head)
-*/
-
- 

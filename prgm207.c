b7f3032681b476b55bb76db08c67129cb872523d
jayeshjeh <jay.parmar.11169@gmail.com>
2021-11-10 19:18:13 +0530

    commit

--- prgm207.c
+++ prgm207.c
@@ -9,22 +9,65 @@ typedef struct node
 
 void Display(PNODE Head)
 {
-
+    while(Head != NULL)
+    {
+        printf("|%d| -> ", Head->data);
+        Head = Head -> next;
+    }
+    printf("NULL\n");
 }
 
 int Count(PNODE Head)
 {
-    return 0;
+    int iCnt =0;
+    while(Head != NULL)
+    {
+        iCnt++;
+        Head = Head->next;
+    }
+    return iCnt;
 }
 
 void InsertFirst(PPNODE Head, int no)
 {
+    PNODE newn = NULL;
+    newn = (PNODE)malloc(sizeof(NODE));
 
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*Head == NULL)
+    {
+        *Head = newn;
+    }
+    else
+    {
+        newn ->next = *Head;
+        *Head = newn;
+    }
 }
 
 void InsertLast(PPNODE Head, int no)
 {
+    PNODE newn = NULL;
+    PNODE temp = *Head;
+    newn = (PNODE)malloc(sizeof(NODE));
 
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*Head == NULL)
+    {
+        *Head = newn;
+    }
+    else
+    {
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newn;
+    }
 }
 
 void DeleteFirst(PPNODE Head)

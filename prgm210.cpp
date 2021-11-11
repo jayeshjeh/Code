5322d7fc06b45b4d5b08d7a90aeb6bc8bbe98f35
jayeshjeh <jay.parmar.11169@gmail.com>
2021-11-11 21:22:43 +0530

    commit

---
+++ prgm210.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+typedef struct node
+{
+    int data;
+    struct node * next;
+}NODE, *PNODE, **PPNODE;
+
+class SinglyLL
+{
+
+};
+
+int main()
+{
+
+    return 0;
+}

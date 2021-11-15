968f24c09342b0a1c16b98ba9bf4177ebb4771df
jayeshjeh <jay.parmar.11169@gmail.com>
2021-11-15 21:30:27 +0530

    commit

--- prgm212.cpp
+++ prgm212.cpp
@@ -21,12 +21,48 @@ public:
         first = NULL;
         last = NULL;
         size =0;
-    }    
+    }  
+
+    void Display()
+    {}
+    int Count()
+    {
+        return size;
+    }  
+    void InsertFirst(int no)
+    {}
+    void InsertLast(int no)
+    {}
+    void InsertAtPos(int no, int ipos)
+    {}
+    void DeleteFirst()
+    {}
+    void DeleteLast()
+    {
+
+    }
+    void DeleteAtPos(int ipos)
+    {
+
+    }
 };
 
 int main()
 {
+    int iret=0;
     SinglyCL obj;
+
+    obj.InsertFirst(51);
+    obj.InsertFirst(21);
+    obj.InsertFirst(11);
+
+    obj.InsertLast(101);
+
+    obj.Display();
+    iret = obj.Count();
+    cout<<"Number of elements are :"<<iret<<"\n";
+
+    return 0;
 
     return 0;
 }

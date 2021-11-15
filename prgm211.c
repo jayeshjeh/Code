deb92878cdb3f0964e024912b0fae109a288f31c
jayeshjeh <jay.parmar.11169@gmail.com>
2021-11-15 19:45:27 +0530

    commit

--- prgm211.c
+++ prgm211.c
@@ -1,4 +1,46 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef
+typedef struct node
+{
+    int data;
+    struct node * next;
+}NODE, *PNODE, **PPNODE;
+
+void InsertFirst(PPNODE Head, int no)
+{
+
+}
+
+void InsertLast(PPNODE Head, int no)
+{
+
+}
+
+void DeleteFirst(PPNODE Head)
+{
+
+}
+
+void DeleteLast(PPNODE Head)
+{
+
+}
+
+void Display(PNODE Head)
+{
+
+}
+
+void Count(PNODE Head)
+{
+    
+}
+
+int main()
+{
+    PNODE First = NULL;
+    PNODE Last = NULL;
+
+    return 0;
+}

57de6db97ef0742f74ac1622f393f4a93fe49de1
jayeshjeh <jay.parmar.11169@gmail.com>
2021-11-15 20:10:33 +0530

    commit

--- prgm211.c
+++ prgm211.c
@@ -7,40 +7,90 @@ typedef struct node
     struct node * next;
 }NODE, *PNODE, **PPNODE;
 
-void InsertFirst(PPNODE Head, int no)
+void InsertFirst(PPNODE Head,PPNODE Tail, int no)
 {
-
-}
-
-void InsertLast(PPNODE Head, int no)
-{
-
-}
-
-void DeleteFirst(PPNODE Head)
-{
-
-}
-
-void DeleteLast(PPNODE Head)
-{
-
-}
-
-void Display(PNODE Head)
-{
-
-}
-
-void Count(PNODE Head)
-{
+    PNODE newn = NULL;
     
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = no;
+    newn->next = NULL;
+
+    if((*Head == NULL) && (*Tail == NULL))
+    {
+        *Head = newn;
+        *Tail = newn;
+    }
+    else
+    {
+        newn->next = *Head;
+        *Head = newn;
+    }
+    (*Tail)->next = *Head;
+}
+
+void InsertLast(PPNODE Head,PPNODE Tail, int no)
+{
+    PNODE newn = NULL;
+    
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = no;
+    newn->next = NULL;
+
+    if((*Head == NULL) && (*Tail == NULL))
+    {
+        *Head = newn;
+        *Tail = newn;
+    }
+    else
+    {
+        (*Tail) ->next = newn;
+        *Tail = newn;
+    }
+    (*Tail)->next = *Head;
+}
+
+void InsertAtPos(PPNODE Head, PPNODE Tail, int no, int ipos)
+{
+
+}
+
+void DeleteFirst(PPNODE Head, PPNODE Tail)
+{
+
+}
+
+void DeleteLast(PPNODE Head, PPNODE tail)
+{
+
+}
+
+void DeleteAtPos(PPNODE Head, PPNODE Tail, int ipos)
+{
+
+}
+
+void Display(PNODE Head, PNODE Tail)
+{
+
+}
+
+int Count(PNODE Head, PNODE Tail)
+{
+
+
+    return 0;
 }
 
 int main()
 {
-    PNODE First = NULL;
-    PNODE Last = NULL;
+    PNODE first = NULL;
+    PNODE last = NULL;
+
+    InsertFirst(&first, &last, 51);
+    InsertFirst(&first, &last, 21);
+    InsertFirst(&first, &last, 11);
 
     return 0;
 }

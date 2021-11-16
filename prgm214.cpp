d0fce453adc47420cdf0e8c9ad22c8ac1c3cf546
jayeshjeh <jay.parmar.11169@gmail.com>
2021-11-16 19:49:14 +0530

    commit

--- prgm214.cpp
+++ prgm214.cpp
@@ -6,9 +6,67 @@ typedef struct node
 {
     int data;
     struct node * next;
-}NODE, *PNODE, **PPNODE;
+    struct node *prev;
+}NODE, *PNODE;
 
-class DoublyCl
+class DoublyCL
 {
+private:
+    PNODE first;
+    PNODE last;
+    PNODE size;
 
+public:
+    DoublyCL()
+    {
+        first = NULL;
+        last = NULL;
+        int size = 0;
+    }
+
+    void Display();
+    int Count();
+    void InsertFirst(int no);
+    void InsertLast(int no);
+    void InsertAtPos(int no,int ipos);
+    void DeleteFirst();
+    void DeleteLast();
+    void DeleteAtPos(int pos);
 };
+
+//Return_Value Class Name :: Function_Name(Parameters)
+void DoublyCL :: InsertFirst(int no)
+{
+    PNODE newn = new NODE;
+
+    newn -> data = no;
+    newn -> next = NULL;
+    newn -> prev = NULL;
+
+    if((first == NULL) && (last == NULL))
+    {
+        first = newn;
+        last = newn;
+    }
+    else
+    {
+        newn -> next = first;
+        first -> prev = newn;
+        first = newn;
+    }
+    last -> next = first;
+    first -> prev = last;
+    size++;
+}
+
+void DoublyCL :: DeleteFirst()
+{
+    
+}
+
+int main()
+{
+    DoublyCL obj;
+
+    return 0;
+}

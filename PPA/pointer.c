07292d4e094e314e5f3d8df4a1150cd0bd6423d6
jayeshjeh <jay.parmar.11169@gmail.com>
2022-02-18 19:17:53 +0530

    commit

--- PPA/pointer.c
+++ PPA/pointer.c
@@ -3,11 +3,18 @@
 int main()
 {
     char cvalue = 'M';
-    int ivalue = 21;
-    float fvalue = 10.11;
-    double dvalue = 20.11;
+    char *cptr  = &cvalue;
 
-    
+
+    int ivalue = 21;
+    int*iptr = &ivalue;
+
+    float fvalue = 10.11;
+    float *fptr = &fvalue;
+
+    double dvalue = 20.11;
+    double *dptr = &dvalue;
 
     return 0;
+
 }

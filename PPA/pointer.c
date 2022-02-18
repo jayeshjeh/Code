cd90725563e325b10e8959cf5e019c61e6b46c3a
jayeshjeh <jay.parmar.11169@gmail.com>
2022-02-18 20:06:07 +0530

    commit

--- PPA/pointer.c
+++ PPA/pointer.c
@@ -15,6 +15,13 @@ int main()
     double dvalue = 20.11;
     double *dptr = &dvalue;
 
+    printf("%c\n", *cptr);
+    printf("%d\n", &cvalue);
+    printf("%d\n", cptr);
+    printf("%d\n", sizeof(cvalue));
+    printf("%d\n", sizeof(cptr));
+    printf("%d\n", sizeof(*cptr));
+
     return 0;
 
 }

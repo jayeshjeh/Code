6e40e2c63ea83a73e4205da03f851013cbe45992
jayeshjeh <jay.parmar.11169@gmail.com>
2022-02-27 10:12:04 +0530

    Dynamic Memory Allocation

--- PPA/dynamic.c
+++ PPA/dynamic.c
@@ -11,14 +11,18 @@ int main()
     printf("Enter number of elements that you want to allocate: ");
     scanf("%d", &size);
 
-    ptr = (int *)malloc(size * sizeof(int));
-    
+    ptr = (int *)malloc(size * sizeof(int));        // 1 :  Allocate the memory
+
     if (ptr == NULL) {
         printf("Unable to allocate memory\n");
     }
     else {
         printf("Memory successfully allocated\n");
     }
+
+    //2 Use the memory 
+
+    free(ptr);          //3 Free the memory
 
     return 0;
 }

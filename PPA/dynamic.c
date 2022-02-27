9b716265658b0c0976c0505c44c71b6997d162df
jayeshjeh <jay.parmar.11169@gmail.com>
2022-02-27 09:22:11 +0530

    Demo static memory allocation

---
+++ PPA/dynamic.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main()
+{
+    int Arr[5];         //Static memort allocation
+    int size = 0;
+
+    printf("Enter number of elements that you want to allocate: ");
+    scanf("%d", %size);
+
+    return 0;
+}

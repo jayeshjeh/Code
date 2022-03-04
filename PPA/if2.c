cd0f3c6df8ee924bd0fdccaabb7c3ac50f031a65
jayeshjeh <jay.parmar.11169@gmail.com>
2022-03-04 20:49:42 +0530

    commit

---
+++ PPA/if2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main() {
+
+    auto int std = 0;
+
+    if(std == 1){
+        printf("Your exam is at 8am\n");
+    }
+    else if(std == 2){
+        printf("Your exam is at 9am\n");
+    }
+    else if(std == 3){
+        printf("Your exam is at 10am\n");
+    }
+    else if(std == 4){
+        printf("Your exam is at 11am\n");
+    }
+    else if(std == 5){
+        printf("Your exam is at 12am\n");
+    }
+    else {
+        printf("Invalid Standard\n");
+    }
+
+    return 0;
+
+}

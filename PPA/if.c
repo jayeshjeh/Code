62977e2519801683ecf5e53683752f8a18bae91c
jayeshjeh <jay.parmar.11169@gmail.com>
2022-03-04 19:42:01 +0530

    commit

---
+++ PPA/if.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int marks = 0;
+
+    printf("Enter of marks\n");
+    scanf("%d", &marks);
+
+    printf("Your entered marks are: %d\n", marks);
+
+    if(marks >= 60){
+        printf("First Class\n");
+    }
+    else {
+        printf("Not a first class\n");
+    }
+
+    return 0;
+}

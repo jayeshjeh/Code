7f83ca848ac09b013422f6441a41bbd317c9cd85
jayeshjeh <jay.parmar.11169@gmail.com>
2022-03-05 09:37:34 +0530

    commit

---
+++ PPA/loop1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    int icnt = 0;
+
+    while (icnt < 5)
+    {
+        printf("Jai Shree Krushna\n");
+        icnt++;
+    }
+
+    int i = 0;
+    do
+    { 
+        printf("Jai Ganesh\n");
+        i++;
+    } while (i < 5);
+    
+    
+    
+
+    return 0;
+}

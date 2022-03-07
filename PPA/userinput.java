7046a1b60c6ce0071ccf3cc53a0984768899cd85
jayeshjeh <jay.parmar.11169@gmail.com>
2022-03-07 10:18:08 +0530

    commit

---
+++ PPA/userinput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class userinput {
+    
+    public static void main(String[] args) {
+        int num;
+        Scanner scan = new Scanner(System.in);
+        System.out.println();
+    }
+
+
+}

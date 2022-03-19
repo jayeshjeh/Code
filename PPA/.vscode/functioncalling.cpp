07286ac42668609d2ee08e24b87598d7136a3ad3
jayeshjeh <jay.parmar.11169@gmail.com>
2022-03-19 11:04:40 +0530

    function call by value, address, reference

--- PPA/.vscode/functioncalling.cpp
+++ PPA/.vscode/functioncalling.cpp
@@ -2,14 +2,17 @@
 using namespace std;
 
 void callValue(int no){
-
+    no++;
 }
+
 
 void callAddress(int *p){
-
+    (*p)++;
 }
 
+
 void callReference(int &ref){
+    ref++;
 
 }
 
@@ -19,8 +22,13 @@ int main(){
     int a = 10, b = 10, c = 10;
 
     callValue(a);
+    cout<<a<<"\n";
+
     callAddress(&b);
+    cout<<b<<"\n";
+
     callReference(c);
+    cout<<c<<"\n";
 
 
     return 0;

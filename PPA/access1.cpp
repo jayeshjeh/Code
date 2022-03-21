150b2fc0dcca24d07acc80aa9d3a556deadc2140
jayeshjeh <jay.parmar.11169@gmail.com>
2022-03-21 22:19:20 +0530

    access1

---
+++ PPA/access1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+class Demo{
+
+    public:
+        int i;
+    private:
+        int j;
+    protected:
+        int k;
+};
+
+
+class Hello : public Demo{
+
+    public:
+        int x;
+    private:
+        int y;
+    protected:
+        int z;
+
+};
+
+
+int main(){
+
+
+
+    return 0;
+
+}

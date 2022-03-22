826e86382db4e9317b16ace8c1b4010531e534df
jayeshjeh <jay.parmar.11169@gmail.com>
2022-03-22 22:30:23 +0530

    commit

--- PPA/static.cpp
+++ PPA/static.cpp
@@ -14,14 +14,18 @@ class Demo{
         }
 };
 
+int Demo::x = 30;
 
 int main(){
+    cout<<Demo::x<<"\n";
 
     Demo obj1;
     Demo obj2;
 
     cout<<sizeof(obj1)<<"\n";
     cout<<sizeof(obj2)<<"\n";
+    cout<<obj1.i<<"\n";
+    cout<<obj2.i<<"\n";
 
     return 0;
 }

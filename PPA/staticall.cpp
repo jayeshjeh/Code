f07dcfd6a0f5f5e77db066fb3b1e05a3f6cb9eea
jayeshjeh <jay.parmar.11169@gmail.com>
2022-03-23 19:35:50 +0530

    static

---
+++ PPA/staticall.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+class Demo{
+    public:
+        int i,j;
+        static int x;
+        Demo(int a = 10, int b = 20){
+            cout<<"Inside constructor\n";
+
+            this->i = a;
+            this->j = b;
+        }
+
+        void fun(int no){
+            cout<<"Inside non static fun\n";
+            cout<<this->i<<"\n";
+            cout<<this->j<<"\n";
+        }
+
+        static void gun(int value){
+            cout<<"Inside static gun\n";
+        }
+};
+
+int Demo::x = 30;
+
+int main(){
+
+    cout<<Demo::x<<"\n";
+    Demo::gun(11);
+
+
+    return 0;
+}
